41db68567c3ce25853a8450f7116ba66fb73dfa4
Yuri Ryzhkov <yura.ryzhkov.photo@yandex.ru>
2025-06-30 17:12:48 +0300

    Ветка users Нужно решить вопрос с подпиской на себя

--- backend/api/serializers/users.py
+++ backend/api/serializers/users.py
@@ -73,6 +73,7 @@ class CustomUserSerializer(UserSerializer):
                 return request.build_absolute_uri(settings.DEFAULT_AVATAR_URL)
             return settings.DEFAULT_AVATAR_URL
 
+
 class AvatarUpdateSerializer(serializers.ModelSerializer):
     avatar = Base64ImageField()
 
