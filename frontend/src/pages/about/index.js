085b507aed88c3a5d64c7bb9c9190aec628dadee
Yuri Ryzhkov <yura.ryzhkov.photo@yandex.ru>
2025-07-13 21:35:53 +0300

    Подключение страниц

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/YuraEtalking/foodgram" className={styles.textLink}>YuraEtalking</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/YuraEtalking" className={styles.textLink}>Юрий Рыжков</a>
             </p>
           </div>
         </aside>
